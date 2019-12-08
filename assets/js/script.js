348aa7d08fb2d7dfd491aefdf40fd5b359ee27e2
Aidesiderio Alhambra <deri.alhambra@gmail.com>
2019-12-07 22:56:16 -0800

    5day forecast added with icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var cnt = "5";
 
 $(document).ready(function(){
 
-  var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+  var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey + "&cnt=5";
    
   
   $.ajax({
@@ -25,6 +25,7 @@ $(document).ready(function(){
     var windDiv = $(".current-wind");
     var humidityDiv = $(".current-humidity");
     var tempDiv = $(".current-temp");
+    var dateDiv = $(".current-date");
 
     var latitude = response.coord.lat;
     var longitude = response.coord.lon; 
@@ -34,7 +35,8 @@ $(document).ready(function(){
     var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 
     cityDiv.append("<h2>" + response.name + "</h2>");
-    tempDiv.append("Current Temp: " + convertKtoF(response.main.temp) + "\xB0" + "F");
+    dateDiv.append("<h4>" + moment(response.coord.dt).format("dddd, MMMM Do, YYYY") + "<h4>");
+    tempDiv.append("Temperature: " + convertKtoF(response.main.temp) + "\xB0" + "F");
     humidityDiv.append("Humidity: " + response.main.humidity + " %");
     windDiv.append("Wind: " + response.wind.speed + "MPH");
     $('#wicon').attr('src', iconurl);    
@@ -60,11 +62,32 @@ $(document).ready(function(){
     
         console.log(fiveDayForecast);
         console.log(response);
+        console.log(moment(response.list[0].dt_txt).format("MMMM Do"));
+
+        for (var i = 3; i < response.list.length; i += 8) {
+          var iconSRC = "http://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png";
+
+          $("<div>").attr("class", "card").attr("id", moment(response.list[i].dt)).appendTo("#fiveDayForecast");
+          $("<h5>").html(moment(response.list[i].dt_txt).format("MMMM Do")).appendTo("#fiveDayForecast");
+          $("<img>").attr("src", iconSRC).appendTo("#fiveDayForecast");
+          $("<p>").html("Temperature: " + convertKtoF(response.list[i].main.temp) + "\xB0" + "F").appendTo("#fiveDayForecast");
+          $("<p>").html(response.list[i].main.humidity + " %").appendTo("#fiveDayForecast");
           
-        var f = 0;
-        var convertKtoF = function(temp){
-          return f= ((temp - 273.15) * 1.80 + 32).toFixed(1);
-        };
+      }; 
+
+
+      // for (var i = 3; i < response.list.length; i += 8) {
+      //     var iconSRC = "http://openweathermap.org/img/w/" + response.list[i].weather[0].id + ".png";
+
+      //     $(".card5day").clone().attr("id", moment(response.list[i].dt))
+      //     .find(".date5").html(moment(response.list[i].dt).format("MMMM Do")).end()
+      //     .find(".icon5").attr("src", iconSRC).end()
+      //     .find(".temp5").html("Temperature: " + convertKtoF(response.list[i].main.temp) + "\xB0" + "F").end()
+      //     .find(".humidity5").html(response.list[i].main.humidity + " %").end()
+      //     .appendTo("#fiveDayForecast");
+      // };          
+               
+        
   
       });
      
